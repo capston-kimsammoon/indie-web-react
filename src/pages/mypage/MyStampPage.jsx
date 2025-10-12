a56ec4480a1f0fc21cf3c6e44a6380dca7b4ae8e
bballangja <165646638+bballangja@users.noreply.github.com>
2025-10-13 00:37:13 +0900

    Update MyStampPage.jsx

--- src/pages/mypage/MyStampPage.jsx
+++ src/pages/mypage/MyStampPage.jsx
@@ -38,7 +38,7 @@ export default function MyStampPage() {
   return (
     <PageWrapper>
       <Header title="내 스탬프" />
-      <div style={{ height: '16px' }} />
+      <div style={{ height: '24px' }} />
       <StampCount>
         <AllText>All</AllText> <CountText>{stamps.length}</CountText>
       </StampCount>
