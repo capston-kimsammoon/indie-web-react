c31a777fb4036cab8f3c733f62edabad90a8ac64
bballangja <165646638+bballangja@users.noreply.github.com>
2025-10-15 11:57:54 +0900

    Update FavoritePage.js

--- src/pages/favorite/FavoritePage.js
+++ src/pages/favorite/FavoritePage.js
@@ -332,5 +332,5 @@ const ScrollableList = styled.div`
 const FavoriteSection = styled.div`
   display: flex;
   flex-direction: column;
-  padding-top: ${({ padded }) => (padded ? '16px' : '4px')};
+  padding-top: ${({ padded }) => (padded ? '16px' : '16px')};
 `;
