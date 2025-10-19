67e3ec027d993abec2b94d205f99931c106d9e8f
rkgmlzla <kim536-9002@naver.com>
2025-10-19 12:22:19 +0900

    feat. 이미지 변경 톱니바퀴 border 색 수정

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -70,7 +70,7 @@ const PrimaryBtn = styled(ModalBtn)`
 `;
 
 const DangerBtn = styled(ModalBtn)`
-  background: #fff; color: #3c9c68; border: 1px solid #ef9a9a;
+  background: #fff; color: #3c9c68; border: 1px solid #368d5dff;
   &:hover { background: #fff5f5; }
 `;
 
